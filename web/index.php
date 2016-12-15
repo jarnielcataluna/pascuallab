397098dee7fef95fe54047258a827f333bb5b964
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2016-12-16 05:03:14 +0800

    demo site ang galing pascual

--- web/index.php
+++ web/index.php
@@ -1,1 +1,410 @@
-<h1>Pascual Labs - Propelrr</h1>
+﻿<!doctype html>
+<html class="no-js" lang="">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>PascualLab | Home</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="css/style.css" media="all"/>
+
+<!--[if lt IE 9]> <script src="js/css3-mediaqueries.js"></script> <![endif]-->
+<script src="js/html5.js"></script>
+<script src="js/lib/modernizr-2.8.3.min.js"></script>
+<script src="js/pace.min.js"></script>
+</head>
+<body>
+<section id="main-container">
+	<header>
+		<div class="container">
+			<div class="social-list fl">
+				<ul>
+					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
+			<div class="search-field fr">
+				<form method="post" action="">
+					<div class="input-wrap">
+						<input type="text" name="" value="" placeholder="Search...">
+						<button><i class="fa fa-search" aria-hidden="true" ></i></button>
+					</div>
+				</form>
+			</div>
+			<div class="logo"><a href=""><img src="images/page_template/logo.png" alt=""></a></div>
+			<div class="clr"></div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Manufacturing &amp; Subsidiaries</a></li>
+					<li><a href="#">Products</a></li>
+					<li><a href="#">Corporate Social Responsibility</a></li>
+					<li><a href="#">Press Room</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section id="main-wrapper">
+		<section id="home" class="home-main-slide">
+			<div class="table-wrap">
+				<div class="table-cell">
+					<div class="container text-center main-text animated hiding" data-animation="fadeInUp" data-delay="400">
+						<h1><span>GALING</span>PascualLab</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi.  Vestibulum id sem justo.</p>
+						<a href="" class="btn btn-orange">Learn More</a>
+					</div>
+				</div>
+			</div>
+			<div id="slider" class="flexslider">
+				<ul class="slides">
+					<li>
+						<div class="hm-slide-main">
+							<div class="coverimage"><img data-src="images/home/slide1.jpg" src="" width="1600" height="857" alt=""></div>
+						</div>
+					</li>
+					<li>
+						<div class="hm-slide-main">
+							<div class="coverimage"><img data-src="images/home/slide2.jpg" src="" width="1600" height="857" alt=""></div>
+						</div>
+					</li>
+					<li>
+						<div class="hm-slide-main">
+							<div class="coverimage"><img data-src="images/home/slide3.jpg" src="" width="1600" height="857" alt=""></div>
+						</div>
+					</li>
+					<li>
+						<div class="hm-slide-main">
+							<div class="coverimage"><img data-src="images/home/slide4.jpg" src="" width="1600" height="857" alt=""></div>
+						</div>
+					</li>
+					<li>
+						<div class="hm-slide-main">
+							<div class="coverimage"><img data-src="images/home/slide1.jpg" src="" width="1600" height="857" alt=""></div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="container">
+				<div id="carousel" class="flexslider animated hiding" data-animation="fadeInUp" data-delay="300">
+					<ul class="slides">
+						<li>
+							<img src="images/home/product1.png" />
+						</li>
+						<li>
+							<img src="images/home/product2.png" />
+						</li>
+						<li>
+							<img src="images/home/product3.png" />
+						</li>
+						<li>
+							<img src="images/home/product4.png" />
+						</li>
+						<li>
+							<img src="images/home/product1.png" />
+						</li>
+					</ul>
+				</div>
+			</div>
+			
+		</section>
+		<div class="subscribe-wrap">
+			<a href="#" class="subscribe-arrow"><img src="images/home/subsriber-arrow.png" alt=""></a>
+			<div class="container">
+				<h3>Subscribe to our newsletter for more information.</h3>
+				<a class="btn btn-ghost">Subscribe Now</a>
+			</div>
+		</div>
+		<section class="sec-wrap h-about">
+			<div class="container">
+				<div class="sec-head animated hiding" data-animation="fadeInUp">
+					<h2 class="sec-title">About Us</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi. Vestibulum id sem justo.</p>
+				</div>
+				<div class="sec-body">
+					<div class="about-list">
+						<ul>
+							<li class="animated hiding" data-animation="fadeInUp" data-delay="100">
+								<div class="about-item">
+									<a href="#">
+										<div class="about-img-wrap"><img src="images/home/bg1.jpg" alt=""></div>
+										<div class="ab-text">
+											<h4>Introduction to<br> PascualLab</h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu,</p>
+										</div>
+									</a>
+								</div>
+							</li>
+							<li class="animated hiding" data-animation="fadeInUp" data-delay="200">
+								<div class="about-item">
+									<a href="#">
+										<div class="about-img-wrap"><img src="images/home/bg2.jpg" alt=""></div>
+										<div class="ab-text">
+											<h4>Mission &amp;<br> Vission</h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu,</p>
+										</div>
+									</a>
+								</div>
+							</li>
+							<li class="animated hiding" data-animation="fadeInUp" data-delay="300">
+								<div class="about-item">
+									<a href="#">
+										<div class="about-img-wrap"><img src="images/home/bg3.jpg" alt=""></div>
+										<div class="ab-text">
+											<h4>Our History &amp;<br> Legacy</h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu,</p>
+										</div>
+									</a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="sec-wrap h-about">
+			<div class="container">
+				<div class="sec-head animated hiding" data-animation="fadeInUp">
+					<h2 class="sec-title">Latest News</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi. Vestibulum id sem justo.</p>
+				</div>
+				<div class="sec-body">
+					<div class="h-news-list">
+						<div class="row">
+							<div class="col-md-8">
+								<div class="featured-new-wrap news-box animated hiding" data-animation="fadeInUp">
+									<div class="fn-img">
+										<a href="#"><img src="images/home/new1.jpg"></a>
+									</div>
+									<div class="fn-content">
+										<div class="fn-date">
+											<span class="fn-month">Dec</span>
+											<span class="fn-num">29</span>
+											<span class="fn-year">2016</span>
+										</div>
+										<div class="fn-text">
+											<h5><a href="#">Payatas residents benefits from PascualLab’s healthcare program</a></h5>
+											<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget. Proin et eros velit. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								 <div class="row">
+								 	<div class="col-md-12">
+								 		<div class="news-box animated hiding" data-animation="fadeInUp">
+								 			<div class="fn-img">
+												<a href="#"><img src="images/home/new2.jpg"></a>
+											</div>
+								 			<div class="fn-content fn-small">
+												<div class="fn-date">
+													<span class="fn-month">Dec</span>
+													<span class="fn-num">08</span>
+													<span class="fn-year">2016</span>
+												</div>
+												<div class="fn-text">
+													<h5><a href="#">Title Goes here 2</a></h5>
+													<p>Fusce nec semper erat. Curabitur lobortis est nisl, at Proin et eros velit. </p>
+												</div>
+											</div>
+								 		</div>
+								 	</div>
+								 	<div class="col-md-12">
+								 		<div class="news-box animated hiding" data-animation="fadeInUp">
+								 			<div class="fn-img">
+												<a href="#"><img src="images/home/new3.jpg"></a>
+											</div>
+								 			<div class="fn-content fn-small">
+												<div class="fn-date">
+													<span class="fn-month">Dec</span>
+													<span class="fn-num">08</span>
+													<span class="fn-year">2016</span>
+												</div>
+												<div class="fn-text">
+													<h5><a href="#">Title Goes here 2</a></h5>
+													<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget.  </p>
+												</div>
+											</div>
+								 		</div>
+								 	</div>
+								 </div>
+							</div>
+						</div>
+
+						<div class="text-center animated hiding" data-animation="fadeInUp">
+							<a href="#" class="btn btn-orange btn-view">View More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="sec-wrap bg-green h-separator">
+			<div class="container animated hiding" data-animation="fadeInUp">
+				<div class="row">
+					<div class="col-sm-10" style="padding-right:30px;">
+						<h3 style="margin-top: 40px;">Making health easier, from our family to yours.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi. Sed blandit, ante eu maximus lobortis, risus arcu ullamcorper urna, ac efficitur felis magna nec leo. Vestibulum id sem justo.</p>
+					</div>
+					<div class="col-sm-2">
+						 <img style="max-width:100%;" src="images/page_template/logo2.png" alt="">
+					</div>
+				</div>
+				
+			</div>
+		</section>
+
+		<section class="sec-wrap">
+			<div class="container">
+
+				<div class="h-box-wrap">
+					<ul>
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="h-box-item">
+								<div class="h-box-img"><img src="images/home/job.jpg" alt=""></div>
+								<div class="h-box-content">
+									<h3>Job</h3>
+									<p>Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi.</p>
+									<a class="btn btn-ghost" href="#">View More</a>
+								</div>
+							</div>
+						</li>
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="h-box-item">
+								<div class="h-box-img"><img src="images/home/partners.jpg" alt=""></div>
+								<div class="h-box-content">
+									<h3>Partnership </h3>
+									<p>Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi.</p>
+									<a class="btn btn-ghost" href="#">View More</a>
+								</div>
+							</div>
+						</li>
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="h-box-item">
+								<div class="h-box-img"><img src="images/home/manufacturing.jpg" alt=""></div>
+								<div class="h-box-content">
+									<h3>Manufacturing</h3>
+									<p>Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi.</p>
+									<a class="btn btn-ghost" href="#">View More</a>
+								</div>
+							</div>
+						</li>
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="h-box-item h-box-green">
+								<div class="h-box-content">
+									<h3>Contact Us</h3>
+									<p>Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi.</p>
+									<a class="btn btn-orange" href="#">View More</a>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<section class="sec-wrap">
+			<div class="container">
+				<div class="sec-head no-border animated hiding" data-animation="fadeInUp">
+					<h2 class="sec-title">Products</h2>
+				</div>
+				<div class="sec-body">
+					<div class="h-product-carousel owl-carousel">
+						<div class="item">
+							<div class="hw-product">
+								<div class="hw-img"><a href="#"><img src="images/home/product1.png" alt=""></a></div>
+								<div class="hw-text">
+									<h5>OraCare</h5>
+									<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget. Proin et eros velit.</p>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="hw-product">
+								<div class="hw-img"><a href="#"><img src="images/home/product2.png" alt=""></a></div>
+								<div class="hw-text">
+									<h5>ASCOF Ponkan</h5>
+									<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget. Proin et eros velit.</p>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="hw-product">
+								<div class="hw-img"><a href="#"><img src="images/home/product4.png" alt=""></a></div>
+								<div class="hw-text">
+									<h5>Poten-cee</h5>
+									<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget. Proin et eros velit.</p>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="hw-product">
+								<div class="hw-img"><a href="#"><img src="images/home/product3.png" alt=""></a></div>
+								<div class="hw-text">
+									<h5>OraCare</h5>
+									<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget. Proin et eros velit.</p>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="hw-product">
+								<div class="hw-img"><a href="#"><img src="images/home/product1.png" alt=""></a></div>
+								<div class="hw-text">
+									<h5>OraCare</h5>
+									<p>Fusce nec semper erat. Curabitur lobortis est nisl, at dapibus dolor dignissim eget. Proin et eros velit.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="text-center">
+						<a href="#" class="btn btn-orange btn-view">View More</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	</section>
+</section>
+<footer>
+	<div class="footer-wrap">
+		<div class="container">
+			<div class="footer-logo text-center"><a href="index.html"><img src="images/page_template/logo.png" alt=""></a></div>	
+			<div class="footer-menu">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Manufacturing &amp; Subsidiaries</a></li>
+					<li><a href="#">Products</a></li>
+					<li><a href="#">Corporate Social Responsibility</a></li>
+					<li><a href="#">Press Room</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi arcu, placerat vel nunc vitae, feugiat tincidunt nisi. Vestibulum id sem justo.</p>
+			</div>
+			<div class="social-list">
+				<ul>
+					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<script src="js/lib/jquery.min.js"></script>
+<script src="js/lib/jquery-ui.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ web/js/custom.js
@@ -0,0 +1,186 @@
+//get all elements with class and get the biggest box
+function get_biggest(elements){
+	var biggest_height = 0;
+	for ( var i = 0; i < elements.length ; i++ ){
+		var element_height = $(elements[i]).height();
+		//compare the height, if bigger, assign to variable
+		if(element_height > biggest_height ) biggest_height = element_height;
+	}
+	return biggest_height;
+}
+
+function resize() {
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+
+	// STICKY FOOTER
+	var headerHeight = $('header').outerHeight();
+	var footerHeight = $('footer').outerHeight();
+	var footerTop = (footerHeight) * -1
+	$('footer').css({marginTop: footerTop});
+	$('#main-wrapper').css({paddingBottom: footerHeight});
+
+	// for vertically middle content
+	$('.bp-middle').each(function() {
+		var bpMiddleHeight = $(this).outerHeight() / 2 * - 1;
+		$(this).css({marginTop: bpMiddleHeight});
+	});
+
+	//$('.coverimage, .home-main-slide .flexslider').css({'height': headerHeight - $('.subscribe-wrap').outerHeight(false)});
+
+	// for equalizer
+	$('.classname').css({minHeight: 0});
+  var ClassName = get_biggest($('.classname'));
+  $('.classname').css({minHeight: ClassName});
+}
+
+$(window).resize(function() {
+	resize();
+	doCoverImage();
+});
+
+$(document).ready(function() {
+	if (Modernizr.touch) {
+		$('html').addClass('bp-touch');
+	}
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	});
+
+	$('.animated').appear(function() {
+        var element = $(this);
+        var animation = element.data('animation');
+        var animationDelay = element.data('delay');
+        if(animationDelay) {
+          setTimeout(function(){
+              element.addClass( animation + " visible" );
+              element.removeClass('hiding');
+          }, animationDelay);
+        } else {
+          element.addClass( animation + " visible" );
+          element.removeClass('hiding');
+        }               
+    }, {accY: -90});
+	
+	resize();
+});
+
+$(window).load(function() {
+	resize();
+});
+
+// preloader once done
+Pace.on('done', function() {
+	// totally hide the preloader especially for IE
+	setTimeout(function() {
+		$('.pace-inactive').hide();
+
+		$('.home-main-slide #slider').flexslider({
+			animation: "fade",
+			directionNav: false,
+			controlNav: true,
+			slideshow: false,
+			slideshowSpeed: 8000, 
+			animationSpeed: 400, 
+			prevText: "&nbsp;",
+			nextText: "&nbsp;",
+			sync: "#carousel",
+			start: function(slider){
+				doCoverImage();
+
+				$('.hm-slide-main').addClass('is-anim');
+				$('header').addClass('is-anim');
+				$('.flex-direction-nav .flex-prev').css({'right' : $('.flex-control-nav.flex-control-paging').width() + 140});
+				setTimeout(function(){
+					$(slider).find("img[data-src]").each(function () {
+			          var src = $(this).attr("data-src");
+			          $(this).attr("src", src).removeAttr("data-src");
+			       });
+				}, 300);
+
+			}
+		});
+
+		$('.home-main-slide #carousel').flexslider({
+		    animation: "slide",
+		    controlNav: false,
+		    directionNav: false,
+		    animationLoop: false,
+		    slideshow: false,
+		    itemWidth: 240,
+		    itemMargin: 5,
+		    asNavFor: '#slider'
+		  });
+		 
+		  
+	}, 500);
+});
+
+function doCoverImage() {
+	$('.coverimage img').each(function() {
+		coverImage( $(this) );
+	});
+}
+
+
+function coverImage( image ) {
+
+	var imgObj = image;
+	var iW = imgObj.attr('width');//width(); //width of image ratio
+	var iH = imgObj.attr('height');//.height(); //height of image ratio
+
+	imgObj.width(0).height(0);
+	
+	var imgContainer = image.parent();
+	var cW = imgContainer.width(); //width of container or browser
+	var cH = imgContainer.height(); //height of container or browser
+
+	//console.log(iW,iH,cW,cH);
+
+	if ( cH > 1 ) {
+		var cP = cW/cH; //ratio of container or browser
+		var iP = iW/iH; //ratio of image
+
+		if ( iP > cP ) { //if image ratio is more than container ratio (if image width is more than container width)
+			iH = cH; //set image height from container height
+			iW = cH * iP; //set image width using container height and image ratio
+			imgObj.css({
+				'margin-top': 0,
+				'margin-left': Math.ceil((cW-iW)/2),
+				'width': Math.ceil(iW),
+				'height': Math.ceil(iH)
+			}); //center the image and set dimensions
+		} else { //if image ratio is less than container ratio (if image height is more than container height)
+			iW = cW; //set image width from container width
+			iH = cW / iP; //set image height from container width and ratio
+			imgObj.css({
+				'margin-top': Math.ceil((cH-iH)/2),
+				'margin-left': 0,
+				'width': Math.ceil(iW),
+				'height': Math.ceil(iH)
+			}); //center the image and set dimensions
+		}
+	} else {
+		imgObj.css({
+			'margin-top': 0,
+			'margin-left': 0,
+			'width': 'auto',
+			'height': 'auto'
+		});
+	}
+}
